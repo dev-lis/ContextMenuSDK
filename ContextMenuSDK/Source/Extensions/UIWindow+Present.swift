1bf502176d1aa1fa9e07ca06da4fde310abc23fb
Aleksandr Lis <a.lis@tinkoff.ru>
2023-08-22 00:58:48 +0700

    find first available to present view controller

---
+++ ContextMenuSDK/Source/Extensions/UIWindow+Present.swift
@@ -0,0 +1,21 @@
+//
+//  UIWindow+Present.swift
+//  ContextMenuSDK
+//
+//  Created by Aleksandr Lis on 22.08.2023.
+//
+
+import UIKit
+
+extension UIWindow {
+    
+    /// Рекурсивно ищем первый жоступный для презентации контроллер
+    func firstViewControllerAvailableToPresent(_ controller: UIViewController? = nil) -> UIViewController? {
+        let rootController = controller ?? rootViewController
+        if let presentedViewController = rootController?.presentedViewController {
+            return firstViewControllerAvailableToPresent(presentedViewController)
+        } else {
+            return rootController
+        }
+    }
+}
